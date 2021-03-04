55038207eb7819b280e3e98dcf276fcf21e719b3
KrysEWilliams <72221785+KrysEWilliams@users.noreply.github.com>
2021-03-04 15:43:57 -0500

    Updated descriptions main

--- simplenetworktopologies/__main__.py
+++ simplenetworktopologies/__main__.py
@@ -27,13 +27,22 @@ class TextDisplay(QtWidgets.QLabel):
 
         self.setText(self.example_text)
 
-        self.generic_text = "generic"
-        self.ring_text = "ring_text"
-        self.bus_text = "bus_text"
-        self.fullyconnected_text = "fullyconnected_text"
-        self.mesh_text = "mesh_text"
-        self.star_text = "star_text"
+         # Krystal: define 6 string variables
+        self.generic_text = "Generic Topologies are just basic setups of any number of nodes/ Computers that are connected in no specific way, and can be changed to accommodate different connections.\n" \
+        "\n" \
+        "You can do print whatever you want here"
 
+        self.ring_text = "Ring Topologies are made of nodes that are connected by edges that pass around a token to receive connection. This is helpful when a Network needs to flow all in one directiont. "
+    
+        self.bus_text = "The Bus Networking topology is built using multiple nodes/ Computers connected to a switch or hub by a single connection cable.This is not too common due to its ability to run slower than normal, because of a high influx of connections all through one single cable."
+    
+        self.fullyconnected_text = "FullyConnected topologies are made of a group of nodes that are each connected to every other node/computer on the network. This allows for fast connections within  a network simultaneously. "
+
+        self.mesh_text = " Mesh is helpful to attend to different needs of a network. Like a hybrid of Generic and Fully Connected topologies that can be used to enstate a network with mixed needs."
+
+        self.star_text = "Star Networking topologies consist of each separate computer/ node that separately connect to a central switch or hub. This is a common topology that allows for continued use, if for some reason one cable or connection fails. "
+        
+        
     def show_text(self, name):
         if name == "Generic":
             self.setText(self.generic_text)
